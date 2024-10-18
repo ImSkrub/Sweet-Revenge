107793deba6a2532014048daa7ba432f82d6172e
ImSkrub <estebanaberastury@gmail.com>
2024-10-18 20:27:26 -0300

    enemigo a rango

--- Sweet Revenge/Assets/Scripts/Factory/Enemy/SpawnerEnemy.cs
+++ Sweet Revenge/Assets/Scripts/Factory/Enemy/SpawnerEnemy.cs
@@ -10,24 +10,44 @@ public class SpawnerEnemy : MonoBehaviour
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] List<string> enemyTypeToSpawn = new List<string>();
     [SerializeField] private int enemiesPerSpawn = 3;
+    private int spawnCount = 0;
 
     [Header("Rondas")]
     [SerializeField] private int currentRound = 1;
-    private int enemiesPerRound;
-
-    
+    [SerializeField] private int enemiesPerRound = 10;
+    [SerializeField] private int roundMultiplier = 2;
+    [SerializeField] private float timeBetweenRounds = 5f; //wait time between rounds
+        
     private void Start()
     {
+        enemiesPerRound = currentRound * roundMultiplier;
         StartCoroutine(SpawnEnemiesRoutine());
     }
-   
+
+    private void Update()
+    {
+        if(enemiesPerRound >= spawnCount)
+        {
+            currentRound++;
+            spawnCount = 0;
+        }
+    }
+
     private IEnumerator SpawnEnemiesRoutine()
     {
-        yield return new WaitForSeconds(spawnInterval);
-        Transform spawnPoint = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Length)];
-        string enemyType = enemyTypeToSpawn[UnityEngine.Random.Range(0, enemyTypeToSpawn.Count)];
-        SpawnEnemies(enemyType, spawnPoint.position);
-        
+        //agregar un while para meterle un valor de cuando tiene que parar, por ejemplo cuando salga del area que pare de spawnear.
+        spawnCount = 0;
+        enemiesPerRound = currentRound * roundMultiplier;
+        while(spawnCount < enemiesPerRound)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            Transform spawnPoint = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Length)];
+            string enemyType = enemyTypeToSpawn[UnityEngine.Random.Range(0, enemyTypeToSpawn.Count)];
+            SpawnEnemies(enemyType, spawnPoint.position);
+            spawnCount += enemiesPerSpawn;
+        }
+        yield return new WaitForSeconds(timeBetweenRounds);
+        currentRound++;
     }
     private void SpawnEnemies(string enemyType, Vector3 position)
     {
