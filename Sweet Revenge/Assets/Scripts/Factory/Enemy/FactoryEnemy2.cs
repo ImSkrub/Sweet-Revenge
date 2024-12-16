65bd4e412b08ccf8359f23179a6af9c3635f36bf
valentrukus <valenrivas2@gmail.com>
2024-12-16 00:38:33 -0300

    spawns

---
+++ Sweet Revenge/Assets/Scripts/Factory/Enemy/FactoryEnemy2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryEnemy2 : MonoBehaviour
+{
+    [SerializeField] private Enemy[] enemies;
+    private Dictionary<string, Enemy> enemiesDictionary = new Dictionary<string, Enemy>();
+
+    private void Awake()
+    {
+        foreach (Enemy enemy in enemies)
+        {
+            enemiesDictionary.Add(enemy.Name, enemy);
+        }
+    }
+
+    public Enemy Create(string id, Transform position)
+    {
+        if (enemiesDictionary.TryGetValue(id, out Enemy enemy))
+        {
+            return Instantiate(enemy, position.position, Quaternion.identity);
+        }
+        return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 1f);
+    }
+
+}
